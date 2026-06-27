bf9059859e81f1ff4706393c693638a710cdf05a
yunjindev <hunterweisenbach@me.com>
2026-06-27 19:57:14 -0400

    code refactor for Hello World with tests

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World NOT LOL"
+	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Fire"))
+	fmt.Println(Hello(""))
 }
